2e38a45a7dd675060aa7b0b3951040e54707c9c0
Soldham1 <s.oldham1204@gmail.com>
2021-01-11 18:55:26 +0000

    Basic form and body parser middleware

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="Email"><br>
+            <input name="password" placeholder="Password"><br>
+            <input name="password2" placeholder="Confirm password"><br>
+            <button>Sign up</button>
+        </form>
+    </div>
+  `);
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Account created");
+});
+
+app.listen(3003, () => {
+  console.log("API running");
+});
