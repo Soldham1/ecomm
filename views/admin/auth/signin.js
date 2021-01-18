031dd822862328f5b490316bcb0d661aa34a0cf3
Soldham1 <s.oldham1204@gmail.com>
2021-01-18 20:32:38 +0000

    Added layout file

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,11 +1,15 @@
+const layout = require("../layout");
+
 module.exports = () => {
-  return `    
-    <div>
+  return layout({
+    content: `
+      <div>
         <form method="POST">
             <input name="email" placeholder="Email"><br>
             <input name="password" placeholder="Password"><br>
             <button>Sign In</button>
         </form>
-    </div>
-    `;
+      </div>
+    `,
+  });
 };
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,17 @@
+const layout = require("../layout");
+
 module.exports = ({ req }) => {
-  return `
-    <div>
-    Your id is: ${req.session.userId}
-        <form method="POST">
-            <input name="email" placeholder="Email"><br>
-            <input name="password" placeholder="Password"><br>
-            <input name="passwordConfirmation" placeholder="Confirm password"><br>
-            <button>Sign up</button>
-        </form>
-    </div>
-    `;
+  return layout({
+    content: `
+        <div>
+            Your id is: ${req.session.userId}
+            <form method="POST">
+                <input name="email" placeholder="Email"><br>
+                <input name="password" placeholder="Password"><br>
+                <input name="passwordConfirmation" placeholder="Confirm password"><br>
+                <button>Sign up</button>
+            </form>
+        </div>
+    `,
+  });
 };
