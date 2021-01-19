3dd7aa21d7f8cd761dc1506edc64bf71a7fe533b
Soldham1 <s.oldham1204@gmail.com>
2021-01-19 20:38:45 +0000

    More changes

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
+const productsRouter = require("./routes/admin/products");
 
 const app = express();
+
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -13,6 +16,7 @@ app.use(
 );
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log("API running");
--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -2,6 +2,17 @@ const { check } = require("express-validator");
 const usersRepo = require("../../repos/user");
 
 module.exports = {
+  requireTitle: check("title")
+    .trim()
+    .isLength({ min: 2, max: 40 })
+    .withMessage("Must be between 2 and 40 characters"),
+
+  requirePrice: check("price")
+    .trim()
+    .toFloat()
+    .isFloat({ min: 1 })
+    .withMessage("Must be greater than 1"),
+
   requireEmail: check("email")
     .trim()
     .normalizeEmail()
